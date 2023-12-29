f94057ade58480cc7749630dca79350d91f67066
rupal3019 <rupal.grover3019@gmail.com>
2023-12-29 08:53:24 +0530

    Added some apis's

--- app/server/database.py
+++ app/server/database.py
@@ -1,4 +1,5 @@
 import motor.motor_asyncio
+from bson.objectid import ObjectId
 
 MONGO_DETAILS = "mongodb://localhost:27017"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
@@ -16,3 +17,63 @@ def student_helper(student) -> dict:
         "year" : student["year"],
         "GPA" : student["gpa"],
     }
+
+
+#Retrieve all students present in the database
+async def retrieve_students():
+    students = []
+    async for student in student_collection.find():
+        students.append(student_helper(student))
+    return students
+
+# Add a new student to the database
+async def add_student(student_data:dict) -> dict:
+    student = student_collection.insert_one(student_data)
+    new_student = await student_collection.find_one({"_id":student.inserted_id})
+    if student:
+        return student_helper(new_student)
+    
+
+
+#Retrieve a student with a matching id
+async def rtrieve_student(id:str) -> dict:
+    student = await student_collection.find_one({"_id":ObjectId(id)})
+    if student:
+        return student_helper(student)
+    
+
+#Update a student with a matching id
+async def update_student(id:str,data:dict):
+    if len(data) < 1:
+        return False
+    student = await student_collection.find_one({"_id":ObjectId(id)})
+    if student:
+        updated_student = student_collection.update_one(
+            {"_id":ObjectId(id)}, {"$set":data}
+        )
+        if updated_student:
+            return True
+        return False
+    
+
+#Delete a student from the database
+async def delete_student(id:str):
+    student = await student_collection.find_one({"_id":ObjectId(id)})
+    if student:
+        await student_collection.delete_one({"_id":ObjectId(id)})
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
